6e3081fb369d84ca3ff43f750ac2fcbf3a1bbed1
PH246 <134579264+PH246@users.noreply.github.com>
2024-01-18 23:12:03 +0700

    Add files via upload

---
+++ yolo_detector.py
@@ -0,0 +1,70 @@
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator, colors
+import cv2
+
+# Load YOLO model
+model = YOLO("yolov8n.pt")
+
+
+def draw_boxes(frame, boxes):
+    """Draw detected bounding boxes on image frame"""
+
+    # Create annotator object
+    annotator = Annotator(frame)
+    for box in boxes:
+        class_id = box.cls
+        class_name = model.names[int(class_id)]
+        coordinator = box.xyxy[0]
+        confidence = box.conf
+
+        # Detect only cat class
+        if class_name == "cat":
+            coordinator = box.xyxy[0]
+
+        # Draw bounding box
+            annotator.box_label(
+            box=coordinator, label=class_name, color=colors(class_id, True)
+            )
+
+    return annotator.result()
+
+
+def detect_object(frame):
+    """Detect object from image frame"""
+
+    # Detect object from image frame
+    results = model.predict(frame)
+
+    for result in results:
+        frame = draw_boxes(frame, result.boxes)
+
+    return frame
+
+
+if __name__ == "__main__":
+    video_path = "CatZoomies.mp4"
+    cap = cv2.VideoCapture(video_path)
+
+    while cap.isOpened():
+        # Read image frame
+        ret, frame = cap.read()
+
+        if ret:
+            # Detect motorcycle from image frame
+            frame_result = detect_object(frame)
+            
+            # inserting text on video 
+            Text = cv2.FONT_HERSHEY_SIMPLEX 
+            cv2.putText(frame, 'Chanatnan-Clicknext-Internship-2024',  (650, 30),  Text, 1,  (255, 0, 0),  2,  cv2.LINE_4) 
+
+            # Show result
+            cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+            cv2.imshow("Video", frame_result)
+            cv2.waitKey(30)
+
+        else:
+            break
+
+    # Release the VideoCapture object and close the window
+    cap.release()
+    cv2.destroyAllWindows()
